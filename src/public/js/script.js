baf879f294aa24e15c20b72d996115fb24706cb1
shetty4l <suyashshetty29@gmail.com>
2019-04-02 20:31:36 -0700

    Landing page modified, more functionality implemented in javascript

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,6 +1,11 @@
-document.getElementById('searchbar').addEventListener('focus', (event) => {
+document.getElementById('searchbar').addEventListener('change', () => {
   const submitBtn = document.getElementById('submit-btn');
-  submitBtn.disabled = false;
+  const searchQuery = document.getElementById('searchbar').value;
+  if(searchQuery!==null && searchQuery!==undefined && searchQuery.length>0) {
+    submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
+  }
 })
 
 let prev = document.getElementById('lucene');
@@ -18,16 +23,33 @@ document.forms.searchQueryForm.addEventListener('submit', async (event) => {
   if(searchQuery!==null && searchQuery!==undefined && searchQuery.length>0) {
     searchQuery = searchQuery.toLowerCase();
 
-      const response = await fetch(`/search?q=${searchQuery}`);
-      const results = await response.json();
-      console.log(results);
+    const response = await fetch(`/search?q=${searchQuery}&algorithm=${getSelectedAlgorithm()}`);
+    const results = await response.json();
+
+    let newsArticlesList = document.getElementById('news-results');
+    let newsArticleListInnerHTML = '';
+    results.map(doc => {
+      newsArticleListInnerHTML += createNewsArticle(doc);
+    });
+    newsArticlesList.innerHTML = newsArticleListInnerHTML;
   }
 });
 
-function getSelectedAlgorithm(form, name) {
-  const radios = form.elements[name];
+function getSelectedAlgorithm() {
+  const radios = document.forms.searchQueryForm.elements['algorithmRadios'];
   for(let i=0; i<radios.length; ++i) {
     if(radios[i].checked) return radios[i].value;
   }
   return null;
+}
+
+function createNewsArticle(doc) {
+  let newsArticleComponentString =
+  `<a href="${doc.url}" target="_blank" class="list-group-item list-group-item-action flex-column align-items-start">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1">${doc.title}</h5>
+    </div>
+    <p class="mb-1">${doc.description}</p>
+  </a>`;
+  return newsArticleComponentString;
 }
