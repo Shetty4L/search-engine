f5114f6bb3bc42bd68dcbae8817274665cb09226
shetty4l <suyashshetty29@gmail.com>
2019-04-02 18:49:39 -0700

    Added root route to HTML landing page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const app = express();
 const router = require('./router.js');
+const app = express();
 
 /* GET home page. */
+app.get('/', (req, res) => {
+  res.sendFile('./src/public/index.html', { root: '.' });
+});
+
+// Route for retrieving search results
 app.get('/search', router.search);
 
 module.exports = app;
