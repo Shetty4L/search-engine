3b1dea7515485b02b179e872a81fcfaa70c67b75
shetty4l <suyashshetty29@gmail.com>
2019-04-19 09:08:07 -0700

    Uncommented all test cases

--- src/routes/__tests__/router.test.js
+++ src/routes/__tests__/router.test.js
@@ -21,118 +21,118 @@ describe('search route', () => {
     }
   };
 
-  // // ----------------------------------- //
-  // describe('not stubbed', () => {
-  //   let res = null;
-  //
-  //   afterEach(() => {
-  //     res = null;
-  //   });
-  //
-  //   describe('error', () => {
-  //     it('throws error if request does not contain 3 parameters', async () => {
-  //       const options = {
-  //         uri: 'http://localhost:3000/search',
-  //         qs: {
-  //           q: 'search query'
-  //         },
-  //         resolveWithFullResponse: true
-  //       };
-  //
-  //       try {
-  //         res = await request.get(options);
-  //       } catch(err) {
-  //         res = err;
-  //       }
-  //       expect(res.statusCode).toEqual(404);
-  //       expect(JSON.parse(res.error)).toEqual({
-  //         error: '3 query parameters are required'
-  //       });
-  //     });
-  //
-  //     it('should return a status code of 404 for invalid query', async () => {
-  //       try {
-  //         res = await makeRequest(null);
-  //       } catch(err) {}
-  //       expect(res.statusCode).toEqual(404);
-  //     });
-  //
-  //     it('returns the correct response object if query term is invalid', async () => {
-  //       try {
-  //         res = await makeRequest(undefined);
-  //       } catch(err) {
-  //         res = err;
-  //       }
-  //       expect(JSON.parse(res.error)).toEqual({
-  //         error: '3 query parameters are required'
-  //       });
-  //     });
-  //   });
-  //
-  //   // --------------------------------- //
-  //   describe('success', () => {
-  //     // ---------------------- //
-  //     jest.setTimeout(10000);
-  //     // ---------------------- //
-  //
-  //     it('should return status code of 200 for valid query', async () => {
-  //       try {
-  //         res = await makeRequest('test');
-  //       } catch(err) {}
-  //       expect(res.statusCode).toEqual(200);
-  //     });
-  //
-  //     it('takes a misspelt query and corrects it if spellCorrect is true', async () => {
-  //       try {
-  //         res = await makeRequest('clmat chnge', true);
-  //       } catch(err) {}
-  //       res = JSON.parse(res.body);
-  //       expect(res.corrected_query).toEqual('climate change');
-  //     });
-  //
-  //     it('takes a misspelt query but doesnt not correct it if spellCorrect is false', async () => {
-  //       try {
-  //         res = await makeRequest('clmat chnge', false);
-  //       } catch(err) {}
-  //       res = JSON.parse(res.body);
-  //       expect(res.corrected_query).toEqual('clmat chnge');
-  //     });
-  //
-  //     it('returns an object containing the original query, corrected query and docs', async () => {
-  //       try {
-  //         res = await makeRequest('test');
-  //       } catch(err) {}
-  //       res = JSON.parse(res.body);
-  //       expect(res).toHaveProperty('original_query');
-  //       expect(res).toHaveProperty('corrected_query');
-  //       expect(res).toHaveProperty('docs');
-  //     });
-  //
-  //     it('returns an array of document objects if query is successful', async () => {
-  //       try {
-  //         res = await makeRequest('clmat chnge');
-  //       } catch(err) {}
-  //       res = JSON.parse(res.body);
-  //       expect(res.docs.length).toBeGreaterThan(0);
-  //     });
-  //
-  //     it('docs have the required values', async () => {
-  //       try {
-  //         res = await makeRequest('test');
-  //       } catch(err) {}
-  //       res = JSON.parse(res.body).docs;
-  //       expect(res[0]).toHaveProperty('id');
-  //       expect(res[0]).toHaveProperty('url');
-  //       expect(res[0]).toHaveProperty('title');
-  //       expect(res[0]).toHaveProperty('description');
-  //       expect(res[0]).toHaveProperty('snippet');
-  //     });
-  //
-  //   });
-  //   // --------------------------------- //
-  //
-  // });
-  // // ----------------------------------- //
+  // ----------------------------------- //
+  describe('not stubbed', () => {
+    let res = null;
+
+    afterEach(() => {
+      res = null;
+    });
+
+    describe('error', () => {
+      it('throws error if request does not contain 3 parameters', async () => {
+        const options = {
+          uri: 'http://localhost:3000/search',
+          qs: {
+            q: 'search query'
+          },
+          resolveWithFullResponse: true
+        };
+
+        try {
+          res = await request.get(options);
+        } catch(err) {
+          res = err;
+        }
+        expect(res.statusCode).toEqual(404);
+        expect(JSON.parse(res.error)).toEqual({
+          error: '3 query parameters are required'
+        });
+      });
+
+      it('should return a status code of 404 for invalid query', async () => {
+        try {
+          res = await makeRequest(null);
+        } catch(err) {}
+        expect(res.statusCode).toEqual(404);
+      });
+
+      it('returns the correct response object if query term is invalid', async () => {
+        try {
+          res = await makeRequest(undefined);
+        } catch(err) {
+          res = err;
+        }
+        expect(JSON.parse(res.error)).toEqual({
+          error: '3 query parameters are required'
+        });
+      });
+    });
+
+    // --------------------------------- //
+    describe('success', () => {
+      // ---------------------- //
+      jest.setTimeout(10000);
+      // ---------------------- //
+
+      it('should return status code of 200 for valid query', async () => {
+        try {
+          res = await makeRequest('test');
+        } catch(err) {}
+        expect(res.statusCode).toEqual(200);
+      });
+
+      it('takes a misspelt query and corrects it if spellCorrect is true', async () => {
+        try {
+          res = await makeRequest('clmat chnge', true);
+        } catch(err) {}
+        res = JSON.parse(res.body);
+        expect(res.corrected_query).toEqual('climate change');
+      });
+
+      it('takes a misspelt query but doesnt not correct it if spellCorrect is false', async () => {
+        try {
+          res = await makeRequest('clmat chnge', false);
+        } catch(err) {}
+        res = JSON.parse(res.body);
+        expect(res.corrected_query).toEqual('clmat chnge');
+      });
+
+      it('returns an object containing the original query, corrected query and docs', async () => {
+        try {
+          res = await makeRequest('test');
+        } catch(err) {}
+        res = JSON.parse(res.body);
+        expect(res).toHaveProperty('original_query');
+        expect(res).toHaveProperty('corrected_query');
+        expect(res).toHaveProperty('docs');
+      });
+
+      it('returns an array of document objects if query is successful', async () => {
+        try {
+          res = await makeRequest('clmat chnge');
+        } catch(err) {}
+        res = JSON.parse(res.body);
+        expect(res.docs.length).toBeGreaterThan(0);
+      });
+
+      it('docs have the required values', async () => {
+        try {
+          res = await makeRequest('test');
+        } catch(err) {}
+        res = JSON.parse(res.body).docs;
+        expect(res[0]).toHaveProperty('id');
+        expect(res[0]).toHaveProperty('url');
+        expect(res[0]).toHaveProperty('title');
+        expect(res[0]).toHaveProperty('description');
+        expect(res[0]).toHaveProperty('snippet');
+      });
+
+    });
+    // --------------------------------- //
+
+  });
+  // ----------------------------------- //
 
   describe('when stubbed', () => {
     beforeEach(() => {
@@ -259,49 +259,49 @@ describe('search route', () => {
 });
 
 describe('suggest route', () => {
-  // // ----------------------------------------------------------------------------- //
-  // const makeRequest = async (query) => {
-  //   const options = {
-  //     uri: 'http://localhost:3000/suggest',
-  //     qs: {
-  //       q: query
-  //     },
-  //     resolveWithFullResponse: true
-  //   };
-  //   try {
-  //     const response = await request.get(options);
-  //     return response;
-  //   } catch(err) {
-  //     return err;
-  //   }
-  // };
-  //
-  // describe('when not stubbed', () => {
-  //   let res = null;
-  //
-  //   afterEach(() => {
-  //     res = null;
-  //   });
-  //
-  //   it('should return a status code of 404 for invalid query', async () => {
-  //     try {
-  //       res = await makeRequest(null);
-  //     } catch(err) {}
-  //     expect(res.statusCode).toEqual(404);
-  //   });
-  //
-  //   it('returns a list of suggestions for an input query', async () => {
-  //     try {
-  //       res = await makeRequest('calif');
-  //     } catch(err) {}
-  //
-  //     expect(res.statusCode).toEqual(200);
-  //     expect(JSON.parse(res.body)).toHaveProperty('suggestions');
-  //     expect(JSON.parse(res.body).suggestions).toHaveLength(5);
-  //     expect(JSON.parse(res.body).suggestions).toEqual([
-  //       'calif', 'california', 'california’s', 'cliff', 'clifford'
-  //     ]);
-  //   });
-  // });
-  // // ----------------------------------------------------------------------------- //
+  // ----------------------------------------------------------------------------- //
+  const makeRequest = async (query) => {
+    const options = {
+      uri: 'http://localhost:3000/suggest',
+      qs: {
+        q: query
+      },
+      resolveWithFullResponse: true
+    };
+    try {
+      const response = await request.get(options);
+      return response;
+    } catch(err) {
+      return err;
+    }
+  };
+
+  describe('when not stubbed', () => {
+    let res = null;
+
+    afterEach(() => {
+      res = null;
+    });
+
+    it('should return a status code of 404 for invalid query', async () => {
+      try {
+        res = await makeRequest(null);
+      } catch(err) {}
+      expect(res.statusCode).toEqual(404);
+    });
+
+    it('returns a list of suggestions for an input query', async () => {
+      try {
+        res = await makeRequest('calif');
+      } catch(err) {}
+
+      expect(res.statusCode).toEqual(200);
+      expect(JSON.parse(res.body)).toHaveProperty('suggestions');
+      expect(JSON.parse(res.body).suggestions).toHaveLength(5);
+      expect(JSON.parse(res.body).suggestions).toEqual([
+        'calif', 'california', 'california’s', 'cliff', 'clifford'
+      ]);
+    });
+  });
+  // ----------------------------------------------------------------------------- //
 })
