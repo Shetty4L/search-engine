34e2191f44572930e693a756648098164f0bb40c
shetty4l <suyashshetty29@gmail.com>
2019-04-19 08:35:07 -0700

    New test cases for snippet generation

--- src/utils/__tests__/utils.test.js
+++ src/utils/__tests__/utils.test.js
@@ -1,6 +1,5 @@
 const htmlParser = require('fast-html-parser');
 const utils = require('../index.js');
-const webpage = require('./webpage.json');
 
 describe('helper functions', () => {
   it('checks if query is valid', () => {
@@ -97,67 +96,99 @@ describe('helper functions', () => {
     const url = './data/abff2f3b-4739-4082-9dc1-97c67af48caa.html';
     it('takes an html page and a single query term and returns a snippet', async () => {
       const query = 'venezuela';
+      let snippet;
       try {
         const DOM = await utils.getHTMLDOMFromUrl(url);
         const sentences = utils.getSentencesFromDOM(DOM);
-        const snippet = utils.generateSnippet(sentences, query);
-        expect(snippet).toEqual('Trump tells Americans not to travel to Venezuela');
+        snippet = utils.generateSnippet(sentences, query);
       } catch(err) {}
+      expect(snippet).toEqual('Trump tells Americans not to travel to Venezuela');
     });
 
     it('takes an html page with a sentence containing all query terms and returns a snippet',
       async () => {
       const query = 'klausner took her sweet revenge';
+      let snippet;
       try {
         const DOM = await utils.getHTMLDOMFromUrl(url);
         const sentences = utils.getSentencesFromDOM(DOM);
-        const snippet = utils.generateSnippet(sentences, query);
-        expect(snippet).toEqual('Today, Klausner took her sweet revenge on the Republican and staunch abortion opponent by raising thousands of dollars for Planned Parenthood');
+        snippet = utils.generateSnippet(sentences, query);
       } catch(err) {}
+      expect(snippet).toEqual('Today, Klausner took her sweet revenge on the Republican and staunch abortion opponent by raising thousands of dollars for Planned Parenthood.');
+    });
+
+    it('takes an html page with a sentence containing all query terms and returns a snippet 2',
+      async () => {
+      const new_url = './data/a6797eaa-4bca-4197-a699-bfff7aa9ea74.html';
+      const query = 'climate change';
+      let snippet;
+      try {
+        const DOM = await utils.getHTMLDOMFromUrl(new_url);
+        const sentences = utils.getSentencesFromDOM(DOM);
+        snippet = utils.generateSnippet(sentences, query);
+      } catch(err) {}
+      expect(snippet).toBe('... what most of the world fears most: the rising tides and temperatures of climate change.');
+    });
+
+    it('takes an html page with a sentence containing all query terms and returns a snippet 3',
+      async () => {
+      const new_url = './data/4c49a8b9-023e-4340-9bd3-693571650bf7.html';
+      const query = 'climate change';
+      let snippet;
+      try {
+        const DOM = await utils.getHTMLDOMFromUrl(new_url);
+        const sentences = utils.getSentencesFromDOM(DOM);
+        snippet = utils.generateSnippet(sentences, query);
+      } catch(err) {}
+      expect(snippet).toBe('Climate change has been the elephant in the room during the past two US presidential debates. Ignoring this issue would be more understandable if this ...');
     });
 
     it('takes an html page with a sentence containing all query terms but not in the same order and returns a snippet 1',
       async () => {
       const query = 'klausner sweet revenge';
+      let snippet;
       try {
         const DOM = await utils.getHTMLDOMFromUrl(url);
         const sentences = utils.getSentencesFromDOM(DOM);
-        const snippet = utils.generateSnippet(sentences, query);
-        expect(snippet).toEqual('Today, Klausner took her sweet revenge on the Republican and staunch abortion opponent by raising thousands of dollars for Planned Parenthood');
+        snippet = utils.generateSnippet(sentences, query);
       } catch(err) {}
+      expect(snippet).toEqual('Today, Klausner took her sweet revenge on the Republican and staunch abortion opponent by raising thousands of dollars for Planned Parenthood.');
     });
 
     it('takes an html page with a sentence containing all query terms but not in the same order and returns a snippet 2',
       async () => {
       const query = 'klausner trolls twitter fun';
+      let snippet;
       try {
         const DOM = await utils.getHTMLDOMFromUrl(url);
         const sentences = utils.getSentencesFromDOM(DOM);
-        const snippet = utils.generateSnippet(sentences, query);
-        expect(snippet).toEqual('Klausner clearly had fun with the trolls, of which there were many, because, you know, Twitter');
+        snippet = utils.generateSnippet(sentences, query);
       } catch(err) {}
+      expect(snippet).toEqual('Klausner clearly had fun with the trolls, of which there were many, because, you know, Twitter.');
     });
 
     it('takes an html page with a sentence containing some query terms and returns a snippet',
       async () => {
       const query = 'john kasich venezuela';
+      let snippet;
       try {
         const DOM = await utils.getHTMLDOMFromUrl(url);
         const sentences = utils.getSentencesFromDOM(DOM);
-        const snippet = utils.generateSnippet(sentences, query);
-        expect(snippet).toEqual('Earlier this month, former Ohio Governor John Kasich refused to sit in a “less desirable seat” and instead took comedian Julie Klausner’s seat on a flight from ...');
+        snippet = utils.generateSnippet(sentences, query);
       } catch(err) {}
+      expect(snippet).toEqual('Earlier this month, former Ohio Governor John Kasich refused to sit in a “less desirable seat” and instead took comedian Julie Klausner’s seat on a flight from ...');
     });
 
     it('takes an html page with a sentence containing no query terms and returns a snippet',
       async () => {
       const query = 'ronald reagan';
+      let snippet;
       try {
         const DOM = await utils.getHTMLDOMFromUrl(url);
         const sentences = utils.getSentencesFromDOM(DOM);
-        const snippet = utils.generateSnippet(sentences, query);
-        expect(snippet).toEqual('');
+        snippet = utils.generateSnippet(sentences, query);
       } catch(err) {}
+      expect(snippet).toEqual('');
     });
   });
 })
